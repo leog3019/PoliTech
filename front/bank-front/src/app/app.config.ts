4ae8e33d0ecd2a9498731d361e46560a70555eab
Leonardo <leonardo.quishpe@epn.edu.ec>
2024-07-10 12:14:05 -0500

    instalacion de material ui

--- front/bank-front/src/app/app.config.ts
+++ front/bank-front/src/app/app.config.ts
@@ -9,7 +9,8 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"bankalao-ed14b","appId":"1:888383840547:web:516e021234315947852c96","databaseURL":"https://bankalao-ed14b-default-rtdb.firebaseio.com","storageBucket":"bankalao-ed14b.appspot.com","apiKey":"AIzaSyCR07ZMbzcqOWwyhoCKIOCBUnsjjOVvrMY","authDomain":"bankalao-ed14b.firebaseapp.com","messagingSenderId":"888383840547","measurementId":"G-NG11S4S6Y0"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"bankalao-ed14b","appId":"1:888383840547:web:516e021234315947852c96","databaseURL":"https://bankalao-ed14b-default-rtdb.firebaseio.com","storageBucket":"bankalao-ed14b.appspot.com","apiKey":"AIzaSyCR07ZMbzcqOWwyhoCKIOCBUnsjjOVvrMY","authDomain":"bankalao-ed14b.firebaseapp.com","messagingSenderId":"888383840547","measurementId":"G-NG11S4S6Y0"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideAnimationsAsync()]
 };
