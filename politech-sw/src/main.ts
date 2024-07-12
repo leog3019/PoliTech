b7f0bff4aaf014c4664d976519a050fe7aa9d824
Leonardo <leonardo.quishpe@epn.edu.ec>
2024-07-12 09:50:19 -0500

    Implementacion de la pagina de login

--- politech-sw/src/main.ts
+++ politech-sw/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { LoginComponent } from '@components/login/login.component';
+import { HeaderComponent } from '@components/Shared-components/header/header.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(LoginComponent)
+  .catch((err) => console.error(err));
+
+  bootstrapApplication(HeaderComponent)
   .catch((err) => console.error(err));
